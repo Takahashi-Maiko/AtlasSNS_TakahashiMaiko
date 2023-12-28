ad63d513ee45c6f01f749fc17a8b946c667aba91
DESKTOP-BK5N0B9\81905 <19960527maiko@gmail.com>
2023-12-28 20:25:08 +0900

    検索機能の実装が出来ない。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,13 +13,35 @@ class UsersController extends Controller
         return view('users.profile');
     }
     public function search(){
-        return view('users.search');
+        //ユーザー一覧をページネートで取得
+        $users = User::paginate(20);
+
+        //viewファイルに変数として渡す
+        return view('users.search')->with([
+            'users'=>$users,
+            'search'=>$search,
+        ]);
     }
 
-    // 新規登録メソッド
-    //     public function profile(){
-    //     return view('users.profile');
-    // }
+    // ユーザー検索の処理の実装
+    public function searchView(Request $request){
+
+        //キーワード取得
+        $keyword = $request->input('keyword');
+
+         // クエリビルダ
+        $query = User::query();
+
+        //キーワードがあった場合
+        if(!empty($keyword)){
+            $query -> orwhere('username','like','%' . $keyword . '%')->get();
+        }
+
+        //全件取得＋ページネーション
+        $data = $query->orderBy('create_at','desc')->paginate(5);
+        return view('users.search')->with('data',$data)->with('keyword',$keyword);
+    }
+
 
     // ログインメソッド
         public function users(){
@@ -27,15 +49,7 @@ class UsersController extends Controller
     }
 
 
-    // ↓↓フォロー機能の実装
-    // ログインしているユーザーがフォローしている人数の取得(フォロー数)
-    // public function follow(){
-    //     $follow = Follows::get();   //followsテーブルからレコードを取得
-    // }
 
-    // //ログインしているユーザーをフォローしている人数の取得(フォロワー数)
-    // public function follower(){
-    //      $follower = Follows::get();   //followsテーブルからレコードを取得
-    // }
+
 
 }
--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -4,4 +4,21 @@
 
 
 
+<form method="GET" action="{{ route('users.index') }}">
+<input type="search" placeholder="ユーザー名を入力" name="search" value="@if (isset($search)) {{ $search }} @endif">
+<button type="submit" class=""> <img src="./images/search.png"></button>
+</form>
+
+<!-- 検索キーワードの表示 -->
+@if(!empty($keyword))
+<p>検索ワード:{{$keyword}}</p>
+@endif
+
+@foreach($users as $user)
+    <a href="{{ route('users.show', ['user_id' => $user->id]) }}">
+        {{ $user->name }}
+    </a>
+@endforeach
+
+
 @endsection
