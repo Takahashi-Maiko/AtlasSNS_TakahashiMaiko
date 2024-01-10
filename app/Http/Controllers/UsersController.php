e58044c5a3d6108de16bd5efc2cfa2c53378056d
DESKTOP-BK5N0B9\81905 <19960527maiko@gmail.com>
2024-01-10 18:41:39 +0900

    ユーザー名を入力しても検索結果が表示されず全件が表示されてしまいます。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,8 @@ class UsersController extends Controller
     public function profile(){
         return view('users.profile');
     }
+
+
     public function search(Request $request){
         //テーブルからすべてのレコードを取得する。
         // $users = Users::query();
@@ -20,22 +22,39 @@ class UsersController extends Controller
         $users = User::paginate(20);
 
         //キーワードがあった場合(2024/1/7追記)
-        $keyword = $request->input('keyword');
-        if(!empty($keyword)){   //$keyword が空でない場合検索処理を実行する。
-            $users -> where('username','like',"%{$keyword}%")
-            ->orwhereHas('username', function ($query) use ($keyword) {
-                $query->where('username', 'LIKE', "%{$keyword}%");
-            })->get();
+        // $keyword = $request->input('keyword');
+        // if(!empty($keyword)){   //$keyword が空でない場合検索処理を実行する。
+        //     $users -> where('username','like',"%{$keyword}%")
+        //     ->orwhereHas('username', function ($query) use ($keyword) {
+        //         $query->where('username', 'LIKE', "%{$keyword}%");
+        //     })->get();
 
+        //追記(2024/1/10)
+        if($users){   //ユーザー名が入力されてたらの処理
+            $keyword = User::where('username','LIKE',"%$users%")->paginate(20);
+            //where()でUserモデルを利用してテーブルのnameカラムを取得
+        }else{   //ユーザー名が入力されていない場合の処理
+            $keyword = User::select('*')->paginate(20);
+            //ユーザー名が入力されていない場合は全件表示させることにする。
+            $users = '全件表示';
         }
 
 
-        //viewファイルに変数として渡す
+                //viewファイルに変数として渡す
         return view('users.search')->with([
             'users'=>$users,
             // 'search'=>$search,
         ]);
+
     }
+
+
+        //viewファイルに変数として渡す
+        // return view('users.search')->with([
+        //     'users'=>$users,
+        //     // 'search'=>$search,
+        // ]);
+
 
     // ユーザー検索の処理の実装
     public function searchView(Request $request){
