7957148e40f8cbd2e36f8d67a10242ca60091343
DESKTOP-BK5N0B9\81905 <19960527maiko@gmail.com>
2024-01-21 16:40:26 +0900

    フォローボタンの設置が出来ない。 正常に機能しない。

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -24,19 +24,36 @@
   <td>{{$user->username}}</td>
   <td><img src="{{$user->images}}" alt="ユーザーアイコン"></td>
 </tr>
+
+<div>
+  <div>{{$user->username}}</div>
+  <button onclick="follow({{ $user->id }})">フォローする</button>
+</div>
+
 @endif
 @endforeach
-    <!-- <a href="">
-        {{ $user->username }}
-    </a> -->
+
+<!-- @foreach($users as $user)
+<div>
+  <div>{{$user->username}}</div>
+  <button onclick="follow({{ $user->id }})">フォローする</button>
+</div>
+@endforeach -->
 
     <!-- ↓↓フォローされているかの判定(2024/1/12) -->
 @if (auth()->user()->isFollowed($user->id))
+<form action="/users/{{$user->id}}/Follow,['id'=>$user->id]"method="POST">
+   <button type="submit" class="unfollow-btn">フォロー解除</button>
+</form>
+@else
+<form action="/users/{{$user->id}}/unFollow,['id'=>$user->id]"method="POST">
+  <button type="submit" class="follow-btn">フォローする</button>
+</form>
 @endif
 
     <!-- ↓↓フォローしているかの判定(2024/1/12) -->
-@if (auth()->user()->isFollowing($user->id))
-@endif
+<!-- @if (auth()->user()->isFollowing($user->id))
+@endif -->
 
 </table>
 </div>
